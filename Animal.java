ea6531882aef1b19c0bc4f5237a08f47d61549cf
Алина Исманова <alinaismanova@MacBook-Air-Alina.local>
2023-12-01 19:12:03 +0300

    Задание 1 по ООП

---
+++ Animal.java
@@ -0,0 +1,61 @@
+import java.time.LocalDate;
+
+public class Animal {
+    public class Task1 {
+
+     
+     private String name;
+     private Illness illness;
+     private String ownerName;
+     public String getOwnerName() {
+        return ownerName;
+    }
+    private LocalDate birthday;
+
+     public LocalDate getBirthday() {
+        return birthday;
+    }
+    public Task1() {
+     }
+     public Task1(String name, Illness illness, String ownerName, LocalDate birthday) {
+         this.name = name;
+         this.illness = illness;
+         this.ownerName = ownerName;
+         this.birthday = birthday;
+     }
+
+     public void setName(String name){
+         this.name = name;
+     }
+     public String getName() {
+         return name;
+     }
+     public Illness getIllness() {
+         return illness;
+     }
+     private void toWakeUp(){
+         System.out.println("Animal woke up");
+     }
+     private void toWakeUp(String time){
+         System.out.println(getClass().getSimpleName() + " woke up is " + time);
+     }
+     private void eat(){
+         System.out.println(getClass().getSimpleName() + " ate");
+     }
+     private void sleep(){
+         System.out.println(getClass().getSimpleName() + " go to bed");
+     }
+     private void play(){
+         System.out.println(getClass().getSimpleName() + " go to play");
+     }
+     public void liveCycle(){
+         toWakeUp();
+         eat();
+         play();
+         sleep();
+     }
+     public void breathe(){
+         System.out.println("breathe");
+     }
+}
+}
---
+++ Chumka.java
@@ -0,0 +1,7 @@
+
+ public class Chumka extends Illness {
+     public Chumka(String illnessName) {
+         super(illnessName);
+     }
+    
+ }
